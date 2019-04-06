393e74b9ee47d55fa1508db95b03b87280e1c75a
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2019-04-06 00:34:46 -0300

    Configurando tema vuetify

--- lembrete/src/plugins/vuetify.js
+++ lembrete/src/plugins/vuetify.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
+import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  primary: colors.cyan.base,
+  secondary: colors.lime.base,
+  accent: colors.orange.base,
+  error: colors.red.base,
+  warning: colors.deepOrange.base,
+  info: colors.lightGreen.base,
+  success: colors.green.base
 })
