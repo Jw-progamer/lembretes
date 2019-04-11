9ca76977b69f3b4646d3aef4947ccd007a6c19e3
Jw-progamer <josiasn.queiroz@gmail.com>
2019-04-10 22:58:20 -0300

    Update firebaseService.js

--- lembrete/src/firebaseService.js
+++ lembrete/src/firebaseService.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase'
 
-const config = {
-  apiKey: 'AIzaSyA_xRShGK_P97gjFKbMxzoat5mWAAXQFlk',
-  authDomain: 'lembretes-edfe0.firebaseapp.com',
-  databaseURL: 'https://lembretes-edfe0.firebaseio.com',
-  projectId: 'lembretes-edfe0',
-  storageBucket: 'lembretes-edfe0.appspot.com',
-  messagingSenderId: '106977370320'
-}
+# TODO: colocar as configurações aqui novamente 
 
 let dbapp = firebase.initializeApp(config)
 
