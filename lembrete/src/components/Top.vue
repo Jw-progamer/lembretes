d080c17cfac7efbbe1352b7baa9832db05b91013
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2019-04-19 15:06:28 -0300

    Lista implementada

--- lembrete/src/components/Top.vue
+++ lembrete/src/components/Top.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-toolbar>
+  <v-toolbar color="primary">
     <v-toolbar-title id="header_sistema" class="header pa-3">Sistema de lembretes do Josias e geomk</v-toolbar-title>
   </v-toolbar>
 </template>
---
+++ lembrete/src/components/lista_lembretes/List_lembrete.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-list three-line>
+        <template v-for="lembrete in projetos">
+            <Lembrete :lembrete=lembrete />
+        </template>
+    </v-list>
+</template>
+<script>
+import Lembrete from "./Lembrete";
+import ftbase from "../../firebaseService";
+
+let db = ftbase.db;
+let projetosRef = db.ref("projetos");
+
+export default {
+  name: "Lista_lembretes",
+  components: {
+    Lembrete
+  },
+  firebase: {
+    projetos: projetosRef
+  }
+};
+</script>
